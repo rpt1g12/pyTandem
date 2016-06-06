af34656124eb9e416dd54761f556ad9ace46e014
R. Perez Torro <rpt1g12@soton.ac.uk>
2016-06-06 14:11:37 +0100

    ThkPd version

--- pyProcess/signalPSD.py
+++ pyProcess/signalPSD.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 from lib.stats import *
 from lib.myPlots import *
 #%%
-sim='A00vClCd'
+plt.close('all')
+sim='4A15pClCd'
+#sim='A00pClCd'
 dataset='clData/6blocks/pvClCd/'+sim+'.dat';
 n,tin,clin,cdin=np.loadtxt(dataset,skiprows=1,unpack=True)
-save=True;scale=False;sclg='spectrum'
+save=False;scale=False;sclg='spectrum'
 
 tmin=0.0;ns=512
 n0=np.where(tin>tmin)[0][0]
@@ -27,14 +29,24 @@ if (scale):
 sin20=np.sin(np.deg2rad(20))
 st=(sin20/0.3)*ff
 #%%
-fig,ax=getFig(sim,211)
+fig,ax=getFig(sim,111)
 ax.loglog(st,Sxx,label='Sxx')
-#ax.set_xlim(0,10)
+
 ax.grid(b=True, which='major', color='k', linestyle='--')
 ax.grid(b=True, which='minor', color='k', linestyle=':')
 ax.set_xlabel(r'$St$')
 ax.set_ylabel(r'$PSD$')
 fit(ax)
-ax2=addAx(fig,212)
-ax2.plot(tn,cdn,label='signal')
-fit(ax2)
+#ax.set_xlim(0,5)
+#ax2=addAx(fig,212)
+#ax2.plot(tn,cdn,label='signal')
+#fit(ax2)
+
+print('Mean='+'{:6.2e}'.format(cdn.mean()))
+print('sigma='+'{:6.2e}'.format(np.sqrt(np.var(cdn))))
+#%%
+name=sim+'PSD'
+if (save==True):
+    plt.sca(ax)
+    path='pgfPlots/'
+    savePlotFile(path=path+name+'.dat',vary=['SCd'],varx=['f'])
